3429c60bbe05e31ccf26c03dc4e8343f5cd90c23
Aurora0895 <alevillara@gmail.com>
2023-01-06 14:41:24 +0100

    fixed abstrac ship
    return to original settings

--- src/Ship.h
+++ src/Ship.h
@@ -8,12 +8,17 @@
 
 class Ship {
 
+protected:
+    bool orientation;
+    int center_X;
+    int center_Y;
+    int dimension;
+    char name;
+    bool *armor;
+
 public:
     Ship(Coordinates first, Coordinates last){};   //to implement on Battle_Ship and Suppport_Ship
     Ship(Coordinates mono_Coord){}; //to implement on Submarine
-    // Aggiungendo = 0 blocco la possibilotà di istanziare oggetti con i seguenti metodi e nel caso 
-    // Decidessi di estendere la classe ho l'obbligo di implementare i metodi.
-    // Nel caso non li implementassi il compilatore di lancia un errore in fase di compilazione 
     virtual void action(Coordinates first, Coordinates last);
     void reset_Armor(bool reset); 
 };
