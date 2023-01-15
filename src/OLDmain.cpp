e8d36f8a8d7c15e65231d1fb0abdd6107f6082e5
Aurora0895 <alevillara@gmail.com>
2023-01-15 13:15:58 +0100

    rename main

---
+++ src/OLDmain.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Com_Player.h"
+#include "tst/test_Com_Player.cpp"
+#include "tst/test_board.cpp"
+#include "tst/test_on_vectors.cpp"
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
+
+
+
+
+
