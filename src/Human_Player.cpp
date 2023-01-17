77c372cd9bd50392d32d3b7b498d616b464552c5
Pidgey27 <galiazzopietro00@gmail.com>
2023-01-17 04:22:12 +0100

    minor changes

--- src/Human_Player.cpp
+++ src/Human_Player.cpp
@@ -1,3 +1,6 @@
+/********************************************
+ *          Pietro Galiazzo 1216517         *
+ ********************************************/
 #include "Human_Player.h"
 
 void Human_Player::prepareBoard(){
