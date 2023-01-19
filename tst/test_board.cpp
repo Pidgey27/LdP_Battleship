c6aae2fc4c82c9836b134b7bcd0b0c30e49acd82
Pidgey27 <galiazzopietro00@gmail.com>
2023-01-19 22:33:00 +0100

    bn

--- tst/test_board.cpp
+++ tst/test_board.cpp
@@ -3,115 +3,105 @@
  ********************************************/
 
 #include "src\Board.h"
-/*
+
 static void testBoard(){
     bool check = true;
     std::cout << "Board Test" << std::endl;
-    //******************************
-    //Check if singleton is correct
-    Board* test = Board::getInstance();
-    Board* test2 = Board::getInstance();
-    if(test != test2)
-        check = false;
+    Board testboard = Board();
 
     //******************************
     //Check if addBattleShip is correct
     // Batleship no collision
     try{
-        test2->addBattleShip(Coordinates("a1"), Coordinates("e1"));
+        testboard.addBattleShip(Coordinates("a1"), Coordinates("e1"));
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 23";
         }
-    
     try{
-        test2->addBattleShip(Coordinates("c5"), Coordinates("g5"));
+        testboard.addBattleShip(Coordinates("c5"), Coordinates("g5"));
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 30";
         }
-    
     try{
-        test2->addBattleShip(Coordinates("l1"), Coordinates("l5"));
+        testboard.addBattleShip(Coordinates("l1"), Coordinates("l5"));
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 37";
-        }
-    
+        } 
     try{
-        test2->addBattleShip(Coordinates("j7"), Coordinates("j11"));
+        testboard.addBattleShip(Coordinates("j7"), Coordinates("j11"));
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 44";
         }
- 
     try{
-        test2->addBattleShip(Coordinates("b10"), Coordinates("f10"));
+        testboard.addBattleShip(Coordinates("b10"), Coordinates("f10"));
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 51";
         }
-
+    //******************************
     // test collision
     try{
-        test2->addBattleShip(Coordinates("d1"), Coordinates("d5"));
+        testboard.addBattleShip(Coordinates("d1"), Coordinates("d5"));
         std::cout << "error line 57";
         check = false;
     }catch(std::invalid_argument e){}
     try{
-        test2->addBattleShip(Coordinates("h9"), Coordinates("l9"));
+        testboard.addBattleShip(Coordinates("h9"), Coordinates("l9"));
         std::cout << "error line 62";
         check = false;
     }catch(std::invalid_argument e){}
     try{
-        test2->addBattleShip(Coordinates("c5"), Coordinates("g5"));
+        testboard.addBattleShip(Coordinates("c5"), Coordinates("g5"));
         std::cout << "error line 67";
         check = false;
     }catch(std::invalid_argument e){}
     try{
-        test2->addBattleShip(Coordinates("d5"), Coordinates("d9"));
+        testboard.addBattleShip(Coordinates("d5"), Coordinates("d9"));
         std::cout << "error line 72";
         check = false;
     }catch(std::invalid_argument e){}
 
-
     //******************************
     //Check if addSupportShip is correct
     try{
-        test2->addSupportShip(Coordinates("k10"), Coordinates("k12"));     
+        testboard.addSupportShip(Coordinates("k10"), Coordinates("k12"));     
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 83";
         }
     try{
-        test2->addSupportShip(Coordinates("h2"), Coordinates("j2"));     
+        testboard.addSupportShip(Coordinates("h2"), Coordinates("j2"));     
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 89";
         }
     try{
-        test2->addSupportShip(Coordinates("h8"), Coordinates("h10"));     
+        testboard.addSupportShip(Coordinates("h8"), Coordinates("h10"));     
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 95";}
     try{
-        test2->addSupportShip(Coordinates("a8"), Coordinates("c8"));
+        testboard.addSupportShip(Coordinates("a8"), Coordinates("c8"));
     }catch(std::invalid_argument e){
         std::cout << "error line 100";
         check = false;}
     // test collisioni
     try{
-        test2->addSupportShip(Coordinates("h8"), Coordinates("j8"));
+        testboard.addSupportShip(Coordinates("h8"), Coordinates("j8"));
         std::cout << "error line 105";
         check = false;
     }catch(std::invalid_argument e){}
     try{
-        test2->addSupportShip(Coordinates("h1"), Coordinates("h3"));
+        testboard.addSupportShip(Coordinates("h1"), Coordinates("h3"));
         std::cout << "error line 110";
         check = false;
     }catch(std::invalid_argument e){}
     try{
-        test2->addSupportShip(Coordinates("a1"), Coordinates("a3"));
+        testboard.addSupportShip(Coordinates("a1"), Coordinates("a3"));
         std::cout << "error line 115";
         check = false;
     }catch(std::invalid_argument e){}
@@ -119,61 +109,63 @@ static void testBoard(){
     //******************************
     //Check if addSupportShip is correct
     try{
-        test2->addSubmarine(Coordinates("e3"));
+        testboard.addSubmarine(Coordinates("e3"));
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 125";
         }
     try{
-        test2->addSubmarine(Coordinates("a6"));
+        testboard.addSubmarine(Coordinates("a6"));
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 131";
         }
     try{
-        test2->addSubmarine(Coordinates("f7"));
+        testboard.addSubmarine(Coordinates("f7"));
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 137";
         }
     try{
-        test2->addSubmarine(Coordinates("l12"));
+        testboard.addSubmarine(Coordinates("l12"));
     }catch(std::invalid_argument e){
         check = false;
         std::cout << "error line 143";
         }
+    //******************************
     // test collision
     try{
-        test2->addSubmarine(Coordinates("h8"));
+        testboard.addSubmarine(Coordinates("h8"));
         std::cout << "error line 148";
         check = false;
     }catch(std::invalid_argument e){}
     try{
-        test2->addSubmarine(Coordinates("i2"));
+        testboard.addSubmarine(Coordinates("i2"));
         std::cout << "error line 153";
         check = false;
     }catch(std::invalid_argument e){}
     try{
-        test2->addSubmarine(Coordinates("j10"));
+        testboard.addSubmarine(Coordinates("j10"));
         std::cout << "error line 158";
         check = false;
     }catch(std::invalid_argument e){}
     try{
-        test2->addSubmarine(Coordinates("a1"));
+        testboard.addSubmarine(Coordinates("a1"));
         std::cout << "error line 163";
         check = false;
     }catch(std::invalid_argument e){}
-
+    //******************************
     //test support function
-    if(test->get(Coordinates("a1")) != 'C'){
+    if(testboard.get(Coordinates("a1")) != 'C'){
         std::cout << "error line 169: get method not working";
         check = false;
     }
 
+
     std::cout << std::endl;
-    test->printBoard();
-    //test->printAtkBoard();
-    //test->printDefBoard();
+    testboard.printBoard();
+    //test.printAtkBoard();
+    //test.printDefBoard();
     if(check)
         std::cout << "all Board test are successfull" << std::endl;
-} */
+}
